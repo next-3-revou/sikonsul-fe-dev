8bb2d3cfcefcfe078ba84443ba73118672897224
Ade Kresna D <45942312+vaiosyndicates@users.noreply.github.com>
2024-04-08 13:02:28 +0700

    temp: temp

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,27 @@ import ReactDOM from 'react-dom/client'
 import {
   BrowserRouter
 } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { Provider } from 'react-redux';
 import App from './App.jsx'
 import './index.css'
 import store from './redux/index.js';
 
+const queryClient = new QueryClient()
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
